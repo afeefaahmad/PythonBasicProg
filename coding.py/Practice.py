d6cad07ef229b8e83b1950ded2d97e2eb08afd33
Afeefa Ahmad <75202156+afeefaahmad@users.noreply.github.com>
2025-02-06 15:09:27 +0530

    Update Practice.py

--- coding.py/Practice.py
+++ coding.py/Practice.py
@@ -14,5 +14,26 @@ is_Palindrome(str)
 #Fibonacci series
 #nth term fibonacci
 
+#Return duplicate elements:
+def find_duplicated(arr):
+    
+    unique = []
+    duplicate = []
+    
+    for element in arr:
+        if element in unique:
+            duplicate.append(element)
+        else:
+            unique.append(element)
+            
+    return duplicate
+    
+    
+arr = [2,1,3,4,5,6,4,3,2,8,9]
+print(find_duplicated(arr))
+
+#Return Unique elements:
+
+
 
     
