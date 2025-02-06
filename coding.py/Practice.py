3d19e12a9847c9fe606f7c3655d5636408293747
Afeefa Ahmad <75202156+afeefaahmad@users.noreply.github.com>
2025-02-06 14:20:34 +0530

    Create Practice.py

---
+++ coding.py/Practice.py
@@ -0,0 +1,16 @@
+
+#Palindrome
+def is_Palindrome(str):
+    if str == str[: :-1]:
+        print("String is Palindrome")
+    else:
+        print("Not")
+        
+    return -1
+
+str = "nitin"
+is_Palindrome(str)
+
+#Fibonacci
+
+    
